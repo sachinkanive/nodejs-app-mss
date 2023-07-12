f3b073924c976158e13ebed338505f5849ba7ef7
Sachin <123356258+sachinkanive@users.noreply.github.com>
2023-07-12 22:05:06 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.8.211/',
+  serverUrl: 'http://172.31.8.211:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_1581729b101574b65e08b40ebf747031f7202329',
+	    'sonar.login': 'squ_ca7aec380dcb403d0140086b6d97bc59b222415c',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
